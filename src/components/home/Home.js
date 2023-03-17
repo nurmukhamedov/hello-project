90a598eb0285b560dfc874ee36e3c21028634771
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-03-17 11:47:52 +0500

    starter file

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import './home.css';
+
+const Home = () => {
+    return (
+        <>
+            <div className='home'>
+                <h1 className='bg-primary'>Home component
+                </h1>            </div>
+        </>
+    )
+}
+export default Home;
